567544ea51ee068b3996726cb75ec93a1499f855
Brenocosta74 <brenolimac17@gmail.com>
2024-08-29 11:07:43 -0300

    adicionado o arquivo dispositivosMoveis.cy.js

---
+++ atividade03/frontend/dispositivosMoveis.cy.js
@@ -0,0 +1,20 @@
+describe('Testando dispositivos móveis', () => {
+    it('Deve existir um menu burguer', () => {
+        cy.visit('/')
+        
+        cy.viewport(550, 750) /* Configura o tamanho da janela para 550px x 750px */
+		cy.viewport('iphone-6') /* Configura o tamanho da janela para 375px x 667px */
+
+        cy.getByData('botao-login').click()
+        cy.getByData('email-input').type('neilton@alura.com')
+        cy.getByData('senha-input').type('123456')
+        cy.getByData('botao-enviar').click()
+        
+        cy.location('pathname').should('eq','/home')
+
+        cy.getByData('menu-burguer').click()
+        cy.getByData('menu-lateral').find('a').eq(3).click()			
+
+        cy.location('pathname').should('eq','/home/investimentos')
+    })
+})
